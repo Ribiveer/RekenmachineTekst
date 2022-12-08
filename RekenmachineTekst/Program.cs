2f7696c9440603ea6d5a4deaf34b106a3c143cdb
Olivia <github@ribiveer.com>
2022-12-08 14:00:10 +0100

    Initial commit

---
+++ RekenmachineTekst/Program.cs
@@ -0,0 +1,40 @@
+﻿
+char[] goodChars = { 'O', 'E', 'Z', 'I', 'G', 'S', 'H', 'B', 'L' };
+
+using(FileStream file = new FileStream("woorden.txt", FileMode.Open))
+{
+
+    using (StreamReader read = new StreamReader(file))
+    {
+        string? lineN;
+        while((lineN = read.ReadLine()) != null)
+        {
+            bool usedAp = false;
+            string line = (string)lineN;
+            line = line.ToUpper();
+            bool goodn = true;
+            for(int i = 0; i < line.Length; i++)
+            {
+                char c = line[i];
+                bool anyGoodChar = false;
+                foreach(char g in goodChars)
+                {
+                    if (c == g)
+                        anyGoodChar = true;
+                }
+                if(c == '\'')
+                {
+                    if (!usedAp)
+                    {
+                        anyGoodChar = true;
+                        usedAp = true;
+                    }
+                }
+                if (!anyGoodChar)
+                    goodn = false;
+            }
+            if(goodn)
+                Console.WriteLine(line);
+        }
+    }
+}
